3a687209e86c7d0f748564054a2caf9a5b555a41
Vicente Reyes <vreyesvaldivieso@gmail.com>
2020-08-25 23:41:22 -0400

    cool wrapping and everything
    Terrible code though

--- Bird.gd
+++ Bird.gd
@@ -3,19 +3,25 @@ extends RigidBody2D
 # Declare member variables here. Examples:
 # var a = 2
 # var b = "text"
+onready var screen = get_viewport_rect().size 
 
-# Called when the node enters the scene tree for the first time.
-func _ready():
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _integrate_forces(state):
-	applied_force = Vector2()
-	if Input.is_action_pressed("ui_left"):
-		applied_force.x = -100
-	if Input.is_action_pressed("ui_right"):
-		applied_force.x = 100
+	wrap()
+	if Input.is_action_just_pressed("ui_left"):
+		add_central_force(Vector2(-100, 0))
+	if Input.is_action_just_released("ui_left"):
+		add_central_force(Vector2(100, 0))
+	if Input.is_action_just_pressed("ui_right"):
+		add_central_force(Vector2(100, 0))
+	if Input.is_action_just_released("ui_right"):
+		add_central_force(Vector2(-100, 0))
 	if Input.is_action_just_pressed("ui_up"):
 		apply_central_impulse(Vector2(0, -100))
 	if Input.is_action_just_pressed("ui_down"):
 		apply_central_impulse(Vector2(0, 100))
+
+func wrap():
+	if screen.x < position.x or position.x < 0:
+		position.x = wrapf(position.x, 0, screen.x)
+	if screen.y < position.y or position.y < 0:
+		position.y = wrapf(position.y, 0, screen.y)
